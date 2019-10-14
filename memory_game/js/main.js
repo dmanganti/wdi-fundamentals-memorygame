b536528aae6eba65d6d155391c5a345e8414a13c
Dann Manganti <dann@Danns-MacBook-Pro.local>
2019-10-14 03:02:12 -0700

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardTwo);
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay);
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2){
+    if(cardsInPlay[0] === cardsInPlay[1]){
+      alert("You found a match!");
+    }
+
+    else{
+      alert("Sorry, try again.");
+    }
+}
